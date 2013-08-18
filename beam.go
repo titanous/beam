04886201463adce14a312114c38427cad2cbc7c8
Solomon Hykes <solomon@dotcloud.com>
2013-08-17 18:48:59 -0700

    Added a placeholder for a beam command-line tool

---
+++ beam.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+// This is a placeholder for the beam command-line tool.
+// It is meant as a convenience to expose capabilities of the protocol and library
+// from the command-line.
+func main() {
+	fmt.Printf("Usage: beam")
+}
