a00aaaf99a72b10d1ca8d25ccbef53b74b564d7d
Solomon Hykes <solomon@dotcloud.com>
2013-08-17 18:59:25 -0700

    Placeholder for beam/client

---
+++ client/client.go
@@ -0,0 +1,9 @@
+// Beam is a protocol and library for service-oriented communication,
+// with an emphasis on real-world patterns, simplicity and not reinventing the wheel.
+//
+// See http://github.com/dotcloud/beam.
+//
+// github.com/dotcloud/beam/client is a client-side implementation of the Beam protocol.
+
+package client
+
