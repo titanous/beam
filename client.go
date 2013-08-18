aaa26fa152b7c6e654f9ce089c691c12620e3a39
Solomon Hykes <solomon@dotcloud.com>
2013-08-18 16:38:33 -0700

    Skeleton client implementation

--- client.go
+++ client.go
@@ -1,1 +1,35 @@
 package beam
+
+import (
+	"io"
+)
+
+type Client struct {
+	Transport io.ReadWriteCloser
+}
+
+func (c *Client) Job(name string, args []string) *Job {
+	return &Job{
+		Name:	name,
+		Args:	args,
+		client:	c,
+	}
+}
+
+type Job struct {
+	Name		string
+	Args		[]string
+	Env		[]string
+	Streams		Streamer
+	Running		bool
+	ExitStatus	int
+	client		*Client
+}
+
+func (job *Job) Start() error {
+	return nil
+}
+
+func (job *Job) Wait() error {
+	return nil
+}
